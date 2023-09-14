6815cf93396dc8e5b6513c3f89a260c7cd29fbfc
jaseeerr <jaseerhasbulla01@gmail.com>
2023-09-14 15:46:57 +0530

    serverUrlUpdated

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
 // export const BASE_URL = "http://localhost:3000/"
-export const BASE_URL = "https://cyberkraft.online/"
+export const BASE_URL = "https://api.socialshots.site/"
 
 
 // export const SOCKET_URL = "http://localhost:3000"
-export const SOCKET_URL = "https://cyberkraft.online"
+export const SOCKET_URL = "https://api.socialshots.site"
 export const IMG_CDN = "https://res.cloudinary.com/dfhcxw70v/image/upload/v1686835716/"
 export const VIDEO_CDN = "https://res.cloudinary.com/dfhcxw70v/video/upload/v1690525487/"
